6d18865019f610b365a7773d4fcc93762b5053c9
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-14 03:11:33 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -122,7 +122,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[2]).to eq(" X | O | O ")
       expect(rows[3]).to eq("-----------")
       expect(rows[4]).to eq(" X | O | O ")
-        
+
 
       board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
 
@@ -137,17 +137,16 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Xs' do
-        board = ["X", "X", "X","X", "X", "X", "X", "X", "X"]
-      output = capture_puts{ display_board(board) }
-      rows = output.split("\n")
+      # Should you want to write your own test for this situation,
+      # read the following code and comments.
 
-      expect(rows[0]).to eq(" X | X | X ")
-      expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq(" X | X | X ")
-      expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq(" X | X | X ")
-    end
-      
+      # Can you copy the syntax of the tests above to write a test for a board
+      # entirely filled with Xs?"
+
+      # Define the board with values that should create the desired output
+      # *** Edit the line below ***
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
+
       # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
@@ -173,24 +172,19 @@ describe "#display_board in 'lib/display_board.rb" do
       # expect(rows[4]).to eq("   |   |   ")
 
       # *** Comment the line below by adding a # at the line start ***
-      # expect(true).to be(true)
-   # end
+      expect(true).to be(true)
+    end
 
     it 'prints an entire board full of Os' do
-       board = ["O", "O", "O","O", "O", "O", "O", "O", "O"]
+      # Can you copy the syntax of the tests above to write a test for a board
+      # entirely filled with Os?
 
-      output = capture_puts{ display_board(board) }
-      rows = output.split("\n")
-
-      expect(rows[0]).to eq(" O | O | O ")
-      expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq(" O | O | O ")
-      expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq(" O | O | O ")
-    end
       # Hint: You should be able to copy the code in the previous it example
       # and make a few simple edits to convert the previous example to this
       # example's situation.
 
       # *** Comment the line below by adding a # at the line start ***
-      # expect(true).to be(true)
+      expect(true).to be(true)
+    end
+  end
+end
